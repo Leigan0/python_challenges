9e92b5427801344df6ae499924fb96cae9274226
Leighann.Gant <leighann.gant@FT-MW5124.local>
2018-09-14 13:59:44 +0100

    app can return fib sequence

--- fibonacci/app/fibonacci.py
+++ fibonacci/app/fibonacci.py
@@ -1,3 +1,18 @@
 class FibonacciChecker():
     def __init__(self, number):
         self.number = number
+    
+    def fib_sequence(self):
+        first = 0
+        second = 1
+        fib_seq = []
+        while first < self.number:
+            print first
+            fib_seq.append(first)
+            next_num = first
+            first = second
+            second = next_num + second
+        return fib_seq
+
+    
+    
--- fibonacci/test/test_fibonacci.py
+++ fibonacci/test/test_fibonacci.py
@@ -3,6 +3,10 @@ from app.fibonacci import FibonacciChecker
 
 class FibonacciCheckerSpec(unittest.TestCase):
 
+    def test_fib_sequence_can_return_fib_sequence(self):
+        fibChecker = FibonacciChecker(8)
+        self.assertEqual(fibChecker.fib_sequence(),[0, 1, 1, 2, 3, 5])
+
     def test_returns_returns_series_of_fibonacci_numbers_until_number(self):
         fibChecker = FibonacciChecker(8)
         self.assertEqual(fibChecker,20)
