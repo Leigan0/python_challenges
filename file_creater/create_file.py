0a174f68957bba3414acd91a1b8259fcf36b96d1
Leighann.Gant <leighann.gant@FT-MW5124.local>
2018-11-09 14:28:25 +0000

    app creates dir if does not exist

--- file_creater/create_file.py
+++ file_creater/create_file.py
@@ -1,4 +1,12 @@
+import os 
+
 class CreateFile():
     def __init__(self, file_path, file_name):
         self.file_path = file_path
         self.file_name = file_name
+    
+    def create_file(self):
+        if os.path.exists(self.file_path): 
+            return True 
+        else: 
+            os.makedirs(self.file_path)
--- file_creater/test_create_file.py
+++ file_creater/test_create_file.py
@@ -1,4 +1,5 @@
 import unittest
+import unittest.mock
 from create_file import CreateFile
 
 class CreateFileTest(unittest.TestCase):
@@ -6,7 +7,28 @@ class CreateFileTest(unittest.TestCase):
         file_path = '/Users/leighann.gant/Projects/code_challenge'
         file_name = 'fun_test.txt'
         self.createFile = CreateFile(file_path, file_name)
+    
 
     def test_it_has_directory_path_and_file_name(self):
         self.assertEqual(self.createFile.file_path, "/Users/leighann.gant/Projects/code_challenge")
         self.assertEqual(self.createFile.file_name, 'fun_test.txt')
+
+    @unittest.mock.patch('os.path.exists')
+    def test_it_checks_if_path_exists(self, mock_os):
+        self.createFile.create_file()
+        self.assertTrue(mock_os.called)
+
+    @unittest.mock.patch('os.makedirs')
+    @unittest.mock.patch('os.path.exists', return_value = False)
+    def test_if_path_does_not_exist_dir_created(self, mock_os, mock_create_dir):
+        self.createFile.create_file()
+        self.assertTrue(mock_create_dir.called)
+    
+    @unittest.mock.patch('os.makedirs')
+    @unittest.mock.patch('os.path.exists', return_value = True)
+    def test_if_path_does_exist_dir_not_created(self, mock_os, mock_create_dir):
+        self.createFile.create_file()
+        self.assertFalse(mock_create_dir.called)
+
+
+
